1680643057e6e439c2a4d7db7ceb8f922433a40f
mazi39662 <76391538+mazi39662@users.noreply.github.com>
2023-08-07 11:17:29 +0800

    Add files via upload

---
+++ controllers/checklist.py
@@ -0,0 +1,74 @@
+def room_checklist():
+    return dict(test="")
+
+
+def rooms():
+    return dict(test="")
+
+
+def ss():
+    return dict(test="")
+
+
+def inspector():
+    return dict(test="")
+
+
+def reminders():
+    return dict(test="")
+
+
+def room_checklist_bankal():
+    return dict(test="")
+
+
+def room_checklist_basak():
+    return dict(test="")
+
+
+def room_checklist_canduman():
+    return dict(test="")
+
+
+def room_checklist_casuntingan():
+    return dict(test="")
+
+
+def room_checklist_carcar():
+    return dict(test="")
+
+
+def room_checklist_consolacion():
+    return dict(test="")
+
+
+def room_checklist_cordova():
+    return dict(test="")
+
+
+def room_checklist_humay2x():
+    return dict(test="")
+
+
+def room_checklist_liloan():
+    return dict(test="")
+
+
+def room_checklist_marigondon():
+    return dict(test="")
+
+
+def room_checklist_manalili():
+    return dict(test="")
+
+
+def room_checklist_naga():
+    return dict(test="")
+
+
+def room_checklist_pusok():
+    return dict(test="")
+
+
+def room_checklist_ramos():
+    return dict(test="")
